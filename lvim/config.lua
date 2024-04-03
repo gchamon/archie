c1623720ed5782f07837928e98667e818c89a080
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2024-04-03 13:55:11 -0300

    add lvim files

---
+++ lvim/config.lua
@@ -0,0 +1,54 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+lvim.format_on_save.enabled = true
+
+lvim.plugins = {
+    {
+        "tpope/vim-surround",
+
+        -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+        -- setup = function()
+        --  vim.o.timeoutlen = 500
+        -- end
+    },
+    {
+        "Mofiqul/vscode.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- local c = require('vscode.colors').get_colors()
+            require('vscode').setup({
+                -- Alternatively set style in setup
+                -- style = 'light'
+
+                -- Enable transparent background
+                -- transparent = true,
+
+                -- Enable italic comment
+                -- italic_comments = true,
+
+                -- Disable nvim-tree background color
+                -- disable_nvimtree_bg = true,
+
+                -- Override colors (see ./lua/vscode/colors.lua)
+                -- color_overrides = {
+                --     vscLineNumber = '#FFFFFF',
+                -- },
+
+                -- Override highlight groups (see ./lua/vscode/theme.lua)
+                -- group_overrides = {
+                --     -- this supports the same val table as vim.api.nvim_set_hl
+                --     -- use colors from this colorscheme by requiring vscode.colors!
+                --     Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+                -- }
+            })
+            require('vscode').load()
+        end,
+    }
+}
