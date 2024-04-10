021a3a7ff62558f33d9f5cd05dc4f1d719436b31
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2024-04-10 07:52:23 -0300

    add comment to magic formula

--- hypr/scripts/confirm-before-exit.sh
+++ hypr/scripts/confirm-before-exit.sh
@@ -6,6 +6,12 @@ EXIT_TYPE="${1:?err}"
 
 calculate_width() {
     type_length="${#1}"
+
+    # this formula is empirical
+    # takes the ceil of division by 2 of the length of the type string and adds to 11
+    # the result is the percentage of the maximum width the window has to occupy
+    # exit = 13%; reboot = 14%; poweroff = 15%;
+    # other modes will scale accordingly
     width=$((11+($type_length+1)/2))%
     echo $width
 }
