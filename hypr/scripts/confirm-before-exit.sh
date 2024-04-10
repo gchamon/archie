de30a3dc7f1ada0934b4ec2bfac1c034b1512f83
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2024-04-10 07:46:26 -0300

    dynamic width; refactor confirm-before-exit.sh

--- hypr/scripts/confirm-before-exit.sh
+++ hypr/scripts/confirm-before-exit.sh
@@ -4,20 +4,24 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 EXIT_TYPE="${1:?err}"
 
-EXIT_ACTION=$({
-    if [[ "$EXIT_TYPE" == "exit" ]]; then
-        echo "$SCRIPT_DIR"/force-exit.sh
-    elif [[ "$EXIT_TYPE" == "poweroff" ]]; then
-        echo sudo poweroff
-    elif [[ "$EXIT_TYPE" == "reboot" ]]; then
-        echo sudo reboot
-    else
-        echo "Action unsupported: $EXIT_TYPE"
-        notify-send "Action unsupported: $EXIT_TYPE"
-        exit 1
-    fi
-})
+calculate_width() {
+    type_length="${#1}"
+    width=$((11+($type_length+1)/2))%
+    echo $width
+}
 
-if [[ "$(rofi -dmenu -p "Confirm $EXIT_TYPE? [y/N]" -theme-str 'listview { enabled: false; } window { width: 13%; } ' | awk '{print tolower($0)}' )" == "y" ]]; then
+if [[ "$EXIT_TYPE" == "exit" ]]; then
+    EXIT_ACTION="$SCRIPT_DIR"/force-exit.sh
+elif [[ "$EXIT_TYPE" == "poweroff" ]]; then
+    EXIT_ACTION="sudo poweroff"
+elif [[ "$EXIT_TYPE" == "reboot" ]]; then
+    EXIT_ACTION="sudo reboot"
+else
+    echo "Action unsupported: $EXIT_TYPE"
+    notify-send "Action unsupported: $EXIT_TYPE"
+    exit 1
+fi
+
+if [[ "$(rofi -dmenu -p "Confirm $EXIT_TYPE? [y/N]" -theme-str "listview { enabled: false; } window { width: $(calculate_width $EXIT_TYPE); }" | awk '{print tolower($0)}' )" == "y" ]]; then
     bash -c "$EXIT_ACTION"
 fi
