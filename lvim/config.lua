7fb2913df919a058fc21d3b13c0fda5f1447da10
Gabriel Chamon Araujo <gchamon@tuta.io>
2024-06-27 08:26:08 -0300

    commiting multiple changes

--- lvim/config.lua
+++ lvim/config.lua
@@ -10,6 +10,44 @@ lvim.format_on_save.enabled = true
 
 lvim.plugins = {
     {
+        "rmagatti/goto-preview",
+        config = function()
+            require('goto-preview').setup {
+                width = 120,             -- Width of the floating window
+                height = 25,             -- Height of the floating window
+                default_mappings = true, -- Bind default mappings
+                debug = false,           -- Print debug information
+                opacity = nil,           -- 0-100 opacity level of the floating window where 100 is fully transparent.
+                post_open_hook = nil     -- A function taking two arguments, a buffer and a window to be ran as a hook.
+                -- You can use "default_mappings = true" setup option
+                -- Or explicitly set keybindings
+                -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+                -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+                -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+            }
+        end
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    -- null_ls.builtins.diagnostics.pylint.with({
+                    --     diagnostics_postprocess = function(diagnostic)
+                    --         diagnostic.code = diagnostic.message_id
+                    --     end,
+                    -- }),
+                    null_ls.builtins.code_actions.refactoring,
+                    -- null_ls.builtins.diagnostics.mypy,
+                    null_ls.builtins.formatting.isort,
+                    -- null_ls.builtins.formatting.black
+                }
+            })
+        end,
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    {
         "ThePrimeagen/refactoring.nvim",
         dependencies = {
             "nvim-lua/plenary.nvim",
@@ -63,3 +101,39 @@ lvim.plugins = {
         end,
     }
 }
+
+local treesitter = require "nvim-treesitter.configs"
+
+treesitter.setup {
+    indent = { enable = true, disable = { "python" } }
+}
+
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--     { name = "black" },
+--     {
+--         name = "prettier",
+--         ---@usage arguments to pass to the formatter
+--         -- these cannot contain whitespace
+--         -- options such as `--line-width 80` become either `{"--line-width", "80"}` or `{"--line-width=80"}`
+--         args = { "--print-width", "100" },
+--         ---@usage only start in these filetypes, by default it will attach to all filetypes it supports
+--         filetypes = { "typescript", "typescriptreact" },
+--     },
+-- }
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+    -- { name = "black" },
+    {
+        name = "shellcheck",
+        args = { "--severity", "warning" },
+    },
+}
+
+-- local code_actions = require "lvim.lsp.null-ls.code_actions"
+-- code_actions.setup {
+--     {
+--         name = "proselint",
+--     },
+-- }
