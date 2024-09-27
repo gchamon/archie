8e6375a57fd7b4de046c0c7815f6c5b162683c77
Gabriel Chamon Araujo <gchamon@tuta.io>
2024-09-27 15:20:40 -0300

    updating and adding multiple files

--- hypr/scripts/screenshot/captureAll.sh
+++ hypr/scripts/screenshot/captureAll.sh
@@ -6,6 +6,7 @@ SCREENSHOT_FILENAME_ABSOLUTE=$HOME/Pictures/Screenshots/$SCREENSHOT_FILENAME
 
 notify-send --app-name=grim --urgency=normal --category=screenshot "Capturing entire screen to $SCREENSHOT_FILENAME_ABSOLUTE"
 grim $SCREENSHOT_FILENAME_ABSOLUTE
+ksnip $SCREENSHOT_FILENAME_ABSOLUTE
 
 # play $HOME/.config/hypr/assets/sounds/camera-shutter.ogg
 
