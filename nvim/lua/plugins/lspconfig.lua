f9e70fe123db2fb29e06db23f32e379e9c8c3c9c
Gabriel Chamon <gchamon@tuta.io>
2025-04-22 10:10:28 -0300

    add lspconfig.lua; add Vertical layout to kitty

---
+++ nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- Useful for debugging formatter issues
+      format_notify = true,
+      inlay_hints = { enabled = false },
+      servers = {
+        bashls = {
+          filetypes = { "sh", "zsh" },
+        },
+      },
+    },
+  },
+}
