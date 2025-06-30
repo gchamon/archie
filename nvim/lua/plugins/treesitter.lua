e618ac179a4d87b79029fbaac64364db75748086
Gabriel Chamon <gchamon@tuta.io>
2025-06-29 21:26:16 -0300

    config elixir in lspconfig properly; add treesitter reqs for elixir

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,4 +3,18 @@ return {
   branch = "master",
   lazy = false,
   build = ":TSUpdate",
+  config = function()
+    treesitter = require("nvim-treesitter.configs")
+    treesitter.setup({
+      ensure_installed = {
+        "eex",
+        "elixir",
+        "erlang",
+        "heex",
+        "html",
+        "surface",
+      },
+      highlight = { enable = true },
+    })
+  end,
 }
