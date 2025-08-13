4ef9f0abffb7017053639b688e6a19f504aa599c
Gabriel Chamon <gchamon@tuta.io>
2025-08-13 17:14:06 -0300

    ksnip already opened, grimblast dies; this closes ksnip before capture

--- hypr/scripts/screenshot/captureArea.sh
+++ hypr/scripts/screenshot/captureArea.sh
@@ -3,4 +3,8 @@
 # grimblast [--notify] [--cursor] [--freeze] [--wait N] [--scale <scale>] (copy|save|copysave|edit) [active|screen|output|area] [FILE|-]
 export GRIMBLAST_EDITOR=ksnip
 
+if pgrep ksnip; then
+    pkill ksnip
+    sleep 1
+fi
 grimblast edit area
