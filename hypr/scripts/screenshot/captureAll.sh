802ceb35b4a4f3a6df0cafcd06f0fe5a257f0420
Gabriel Chamon <gchamon@tuta.io>
2026-03-02 11:25:11 -0300

    wait a bit to send nofitication on captureall
    otherwise it'll capture the notification fading in

--- hypr/scripts/screenshot/captureAll.sh
+++ hypr/scripts/screenshot/captureAll.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 SCREENSHOT_FILENAME=$(date +'%Y-%m-%dT%H:%M:%S%z_grim.png')
 SCREENSHOT_FILENAME_ABSOLUTE=$HOME/Pictures/Screenshots/$SCREENSHOT_FILENAME
 
-notify-send --app-name=grim --urgency=normal --category=screenshot "Capturing entire screen to $SCREENSHOT_FILENAME_ABSOLUTE"
+sleep 1 && notify-send --app-name=grim --urgency=normal --category=screenshot "Capturing entire screen to $SCREENSHOT_FILENAME_ABSOLUTE" &
+
 grim $SCREENSHOT_FILENAME_ABSOLUTE
 ksnip $SCREENSHOT_FILENAME_ABSOLUTE
 
 # play $HOME/.config/hypr/assets/sounds/camera-shutter.ogg
-
