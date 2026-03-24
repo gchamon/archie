6b5f2c6c2c4bcbe46ec0286cc1b7eabd73e88725
Gabriel Chamon <gchamon@tuta.io>
2026-03-24 10:38:56 -0300

    fix captureArea screenshot script

--- deployment-packages/config/hypr/scripts/screenshot/captureArea.sh
+++ deployment-packages/config/hypr/scripts/screenshot/captureArea.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# grimblast [--notify] [--cursor] [--freeze] [--wait N] [--scale <scale>] (copy|save|copysave|edit) [active|screen|output|area] [FILE|-]
-export GRIMBLAST_EDITOR=ksnip
+SCREENSHOT_FILENAME=$(date +'%Y-%m-%dT%H:%M:%S%z_grim.png')
+SCREENSHOT_FILENAME_ABSOLUTE=$HOME/Pictures/Screenshots/$SCREENSHOT_FILENAME
 
-if pgrep ksnip; then
-    pkill ksnip
-    sleep 1
-fi
-grimblast edit area
+grimblast save area $SCREENSHOT_FILENAME_ABSOLUTE
+sleep 1 && notify-send --app-name=grim --urgency=normal --category=screenshot "Capturing area to $SCREENSHOT_FILENAME_ABSOLUTE"
+ksnip -e ~/Pictures/Screenshots/2026-03-24T10:36:16-0300_grim.png
