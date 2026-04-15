243932251ec803b2a07fc150c58601c2156de71d
Gabriel Chamon <gchamon@tuta.io>
2026-04-15 13:09:07 -0300

    fix google-gemini-cli provider instead of just google

--- deployment-packages/local/lib/zsh/commands-agents.sh
+++ deployment-packages/local/lib/zsh/commands-agents.sh
@@ -3,4 +3,4 @@
 # oh-my-pi aliases
 alias omp:openai="omp --smol openai/gpt-5.4-nano --slow openai/gpt-5.4-pro --plan openai/gpt-5.4-pro --default openai/gpt-5.4 "
 alias omp:anthropic="omp --smol anthropic/claude-haiku-4-5 --slow anthropic/claude-opus-4-6 --plan anthropic/claude-opus-4-6 --model anthropic/claude-sonnet-4-6 "
-alias omp:google="omp --smol google/gemini-3.1-flash-lite-preview --slow google/gemini-3.1-pro-preview --plan google/gemini-3.1-pro-preview --model google/gemini-3-flash-preview "
+alias omp:google="omp --smol google-gemini-cli/gemini-3.1-flash-lite-preview --slow google-gemini-cli/gemini-3.1-pro-preview --plan google-gemini-cli/gemini-3.1-pro-preview --model google-gemini-cli/gemini-3-flash-preview "
