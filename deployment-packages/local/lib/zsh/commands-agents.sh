a4c399f88fab630f8fbe654ed16df3dfa3bf691d
Gabriel Chamon <gchamon@tuta.io>
2026-04-15 18:57:44 -0300

    fix default model

--- deployment-packages/local/lib/zsh/commands-agents.sh
+++ deployment-packages/local/lib/zsh/commands-agents.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 # oh-my-pi aliases
-alias omp:openai="omp --thinking medium --smol openai/gpt-5.4-nano --slow openai/gpt-5.4-pro --plan openai/gpt-5.4-pro --default openai/gpt-5.4 "
+alias omp:openai="omp --thinking medium --smol openai/gpt-5.4-nano --slow openai/gpt-5.4-pro --plan openai/gpt-5.4-pro --model openai/gpt-5.4 "
 alias omp:anthropic="omp --thinking medium --smol anthropic/claude-haiku-4-5 --slow anthropic/claude-opus-4-6 --plan anthropic/claude-opus-4-6 --model anthropic/claude-sonnet-4-6 "
 alias omp:google="omp --thinking high --smol google-gemini-cli/gemini-3.1-flash-lite-preview --slow google-gemini-cli/gemini-3.1-pro-preview --plan google-gemini-cli/gemini-3.1-pro-preview --model google-gemini-cli/gemini-3-flash-preview "
