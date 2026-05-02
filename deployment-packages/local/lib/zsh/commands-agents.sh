b8e1a7b025a9f81634f6df639f38a6a1c4d7c842
Gabriel Chamon <gchamon@tuta.io>
2026-05-02 13:57:56 -0300

    update omp:google

--- deployment-packages/local/lib/zsh/commands-agents.sh
+++ deployment-packages/local/lib/zsh/commands-agents.sh
@@ -3,5 +3,5 @@
 # oh-my-pi aliases
 alias omp:openai="omp --thinking medium --smol openai/gpt-5.4-nano --slow openai/gpt-5.4-pro --plan openai/gpt-5.4-pro --model openai/gpt-5.4 "
 alias omp:anthropic="omp --thinking medium --smol anthropic/claude-haiku-4-5 --slow anthropic/claude-opus-4-6 --plan anthropic/claude-opus-4-6 --model anthropic/claude-sonnet-4-6 "
-alias omp:google="omp --thinking high --smol google-gemini-cli/gemini-3.1-flash-lite-preview --slow google-gemini-cli/gemini-3.1-pro-preview --plan google-gemini-cli/gemini-3.1-pro-preview --model google-gemini-cli/gemini-3-flash-preview "
+alias omp:google="omp --smol google-gemini-cli/gemini-3.1-flash-lite-preview:high --slow google-gemini-cli/gemini-3.1-pro-preview:high --plan google-gemini-cli/gemini-3.1-pro-preview --model google-gemini-cli/gemini-3-flash-preview "
 alias omp:kimi="omp --smol kimi-code/kimi-k2-turbo-preview --slow kimi-code/kimi-for-coding:high --plan kimi-code/kimi-for-coding:medium --model kimi-code/kimi-for-coding:low"
