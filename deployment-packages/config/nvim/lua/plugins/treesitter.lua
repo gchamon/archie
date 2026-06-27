a42666727705fb9a08a7ffd27da4c8d378561493
Gabriel Chamon <gchamon@tuta.io>
2026-06-27 16:48:43 -0300

    Configure Neovim language support
    - Prefer csharp_ls for C# files
    
    - Enable GDScript LSP via Godot
    
    - Install C# and GDScript Tree-sitter parsers
    
    - Ignore local Waybar config and style overrides

--- deployment-packages/config/nvim/lua/plugins/treesitter.lua
+++ deployment-packages/config/nvim/lua/plugins/treesitter.lua
@@ -2,9 +2,11 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     vim.list_extend(opts.ensure_installed, {
+      "c_sharp",
       "eex",
       "elixir",
       "erlang",
+      "gdscript",
       "heex",
       "surface",
     })
